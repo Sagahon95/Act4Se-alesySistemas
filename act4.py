81e7673c686b58d17980ac461e5b068a27b5a2d9
Sagahon95 <eduardo.hdz.sag@gmail.com>
2025-07-22 12:21:42 -0600

    Paso 2: señal modulada en amplitud agregada

--- act4.py
+++ act4.py
@@ -19,3 +19,23 @@ plt.grid(True)
 plt.tight_layout()
 plt.show()
 
+# Parámetros de la portadora
+fc = 1000           # Frecuencia de la portadora (Hz)
+Ac = 1              # Amplitud de la portadora
+
+# Señal portadora
+portadora = Ac * np.cos(2 * np.pi * fc * t)
+
+# Señal modulada (AM doble banda con portadora)
+modulada = (1 + mensaje) * portadora
+
+# Graficar la señal modulada
+plt.figure(figsize=(10, 4))
+plt.plot(t[:1000], modulada[:1000])  # Mostramos solo una parte para que se vea bien
+plt.title('Señal Modulada en Amplitud (AM)')
+plt.xlabel('Tiempo [s]')
+plt.ylabel('Amplitud')
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
